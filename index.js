28ee02f1d786c5f62a2dca87ea6f6971e9ed94d3
Abhinay-cqss <abhinay@cqs.in>
2020-11-10 11:40:27 +0530

    socket version change

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-const express = require('express');
-const http = require('http');
-const socketio = require('socket.io');
 require('dotenv').config();
-
-const app = express();
-const server = http.createServer(app);
-const io = socketio(server);
-const router = require('./routes');
 const PORT = process.env.PORT || 5000;
-
-io.on('connection', (socket) => {
-  console.log("Socket: We have a new connection!");
-  socket.on("Socket: Disconnected");
-});
-
-app.use(router);
-
-server.listen(PORT, () => {
+const express = require('express');
+const cors = require('cors');
+const router = require('./routes');
+const socket = require('socket.io');
+const app = express();
+app.use(cors());
+app.use(express.json());
+// app.use(router)
+const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+const io = socket(server);
+io.on('connection', (socket) => {
+  console.log("Socket: We have a new connection!");
+  socket.on('join',(data,callBack)=>{
+    console.log(data)
+    callBack({message: "abhinay is awesome"});;
+  })
+  socket.on("disconnect",()=>{
+    console.log('lost');
+  });
+});
+
+
 
